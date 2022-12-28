9f3cf5debc2095b6f33b9105ff3c62ecc4c7dfa6
nalee97 <mohamednaleef1564@gmail.com>
2022-12-28 10:08:16 +0530

    commit

--- FastestFilteringMethod/Program.cs
+++ FastestFilteringMethod/Program.cs
@@ -5,21 +5,31 @@ using FastestFilteringMethod;
 
 Console.WriteLine("Testing the fastest way to filter from the list");
 
-var listOfStudents = new Faker<Employee>()
+var listOfEmployees = new Faker<Employee>()
     .RuleFor(e => e.Id, f => f.Random.Guid())
     .RuleFor(e => e.Name, f => f.Name.FirstName())
     .RuleFor(e => e.Surname, f => f.Name.LastName())
-    .RuleFor(e => e.Age, f => f.Random.Int(5, 18))
+    .RuleFor(e => e.Age, f => f.Random.Int(25, 40))
     .RuleFor(e => e.Email, f => f.Internet.Email())
     .RuleFor(e => e.Phone, f => f.Phone.PhoneNumber()
     ).Generate(1000);
 
-//print last 10 students from the list
-Console.WriteLine("Print last 10 students from the list");
 
-foreach(var employee in listOfStudents.Skip(listOfStudents.Count-10))
+/*
+//print last 10 employees from the list
+Console.WriteLine("Print last 10 employees from the list");
+
+foreach(var employee in listOfEmployees.Skip(listOfEmployees.Count-10))
 {
     Console.WriteLine("First name : " + employee.Name + "Last name : " +employee.Surname);
 }
 
 Console.ReadLine();
+*/
+
+//filter employee ages between 25-30
+
+var FilteredEmployee =listOfEmployees.Where(e =>e.Age >=  25 && e.Age <= 30).ToList();
+
+//print the count of filtered employee list
+Console.WriteLine($"Count of filtered Employee list : {FilteredEmployee.Count}");
