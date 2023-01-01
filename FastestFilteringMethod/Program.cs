b76b2ea57bf0a273cc87c865161ef9e7e4e4ba32
nalee97 <mohamednaleef1564@gmail.com>
2023-01-01 18:42:24 +0530

    finished

--- FastestFilteringMethod/Program.cs
+++ FastestFilteringMethod/Program.cs
@@ -13,29 +13,54 @@ using System.Text;
 
 
 
-
-
-
-//Array of Random Alphabetical letter char array
-/*
 var randomAlphabeticalLetters = new Faker().Random.AlphaNumeric(1000).ToCharArray();
 
-var stopWatch = new Stopwatch();
-stopWatch.Start();
+Console.WriteLine("Print 1000 Random letters");
 
-foreach (var c in randomAlphabeticalLetters)
-{
-    Console.Write(c + " ");
-}
+var Stopwatch = new Stopwatch();
 
-
+//First Method
+Stopwatch.Start();
 Array.Sort(randomAlphabeticalLetters);
 
-stopWatch.Stop();
+Stopwatch.Stop();
+Console.WriteLine($"Time taken by array.sort method : {Stopwatch.ElapsedTicks} ticks");
+Console.WriteLine($"Time taken by array.sort method : {Stopwatch.ElapsedMilliseconds} ms");
 
-Console.WriteLine($"Time taken by Where method: {stopWatch.ElapsedTicks} ticks");
 
-*/
+//Second Method
+Stopwatch.Restart();
+var result = from name in randomAlphabeticalLetters orderby name select name;
+
+Stopwatch.Stop();
+Console.WriteLine($"Time Taken By Query.next method : {Stopwatch.ElapsedTicks} ticks");
+Console.WriteLine($"Time taken by Query method : {Stopwatch.ElapsedMilliseconds} ms");
+/*foreach (var letters in result)
+{
+    Console.WriteLine(letters);
+}*/
+
+
+
+//Third Method
+Stopwatch.Restart();
+var result01 = new List<char>(randomAlphabeticalLetters);
+result01.Sort();
+
+Stopwatch.Stop();
+Console.WriteLine($"Time Taken By LINQ Query Method : {Stopwatch.ElapsedTicks} ticks");
+Console.WriteLine($"Time taken by LiNQ method : {Stopwatch.ElapsedMilliseconds} ms");
+
+/*foreach (var word in result01)
+{
+    Console.WriteLine(word);
+}*/
+//Console.WriteLine($"Count of letters: {randomAlphabeticalLetters.Length}");
+
+//Console.WriteLine(randomAlphabeticalLetters);
+Console.ReadLine();
+
+
 
 
 
